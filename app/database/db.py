898c36bda8720d2d65e1701f13e3f15bf1b7639e
Oumaima1mal <oumaimamaliki2003@gmail.com>
2025-05-27 16:20:41 +0100

    correction

--- app/database/db.py
+++ app/database/db.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:2003@localhost:5432/GestionTACHE"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
